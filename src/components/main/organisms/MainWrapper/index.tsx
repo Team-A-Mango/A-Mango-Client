16dcfe55111428845cbc5a97f8264983c05aa9cf
pki071120 <ddametyy@gmail.com>
2024-12-17 14:46:46 +0900

    Apply Header to MainPage

--- src/components/main/organisms/MainWrapper/index.tsx
+++ src/components/main/organisms/MainWrapper/index.tsx
@@ -4,10 +4,12 @@ import GoodsList from '../../molecules/GoodsList'
 import * as S from './style'
 import Banner from '@/assets/png/Banner.png'
 import Footer from '@/components/common/footer'
+import Header from '@/components/Header'
 
 const MainWrapper = () => {
   return (
     <S.Wrapper>
+      <Header />
       <S.Content>
         <S.Container>
           <Image src={Banner} alt='베너 사진' />
