b1c1213bb5a39999cd62ef05f84351c6ecac5b76
pki071120 <ddametyy@gmail.com>
2024-12-17 20:02:00 +0900

    Add Filter Component

---
+++ src/components/Filter/style.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled'
+
+export const Filter = styled.div`
+  display: flex;
+  padding: 0.625rem 1.25rem;
+  align-items: center;
+  gap: 0.875rem;
+  border-radius: 1rem;
+  border: 1px solid #ececec;
+  background: #fff;
+`
