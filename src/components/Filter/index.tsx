778630254779f6e0a8d834b2c87c11e16c0c73dc
pki071120 <ddametyy@gmail.com>
2024-12-17 20:06:37 +0900

    Add Filter Component

---
+++ src/components/Filter/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import * as S from './style'
+import { FilterIcon } from '@/assets/svg'
+
+const Filter = () => {
+  return (
+    <S.Filter>
+      <FilterIcon /> 필터
+    </S.Filter>
+  )
+}
+
+export default Filter
