a2ed9d3ec5543263be5246ab6907534aa281da5e
pki071120 <ddametyy@gmail.com>
2024-12-19 10:16:34 +0900

    edit axios instance

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,13 @@
 import axios from 'axios'
 
-const token = localStorage.getItem('accessToken')
+const getToken = () => {
+  if (typeof window !== 'undefined') {
+    return localStorage.getItem('accessToken')
+  }
+  return null
+}
+
+const token = getToken()
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
 
 export const baseInstance = axios.create({
