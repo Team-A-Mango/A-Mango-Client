aa8f9b455495ddcd53ed3547bbb983b2310e365b
pki071120 <ddametyy@gmail.com>
2024-12-19 15:43:17 +0900

    Update PriceContainer component

--- src/components/goods-detail/molecules/PriceContainer/index.tsx
+++ src/components/goods-detail/molecules/PriceContainer/index.tsx
@@ -3,11 +3,11 @@ import Price from '../../atoms/Price'
 import * as S from './style'
 import Button from '@/components/common/Button'
 
-const PriceContainer = () => {
+const PriceContainer = ({ price }: { price: number }) => {
   return (
     <S.Wrappeer>
       <S.PriceText>가격</S.PriceText>
-      <Price />
+      <Price price={price} />
       <Button text='구매하기' />
     </S.Wrappeer>
   )
