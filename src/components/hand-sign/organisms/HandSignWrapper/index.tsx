f96f2c150d080fddbf38520e41c279465095e34c
pki071120 <ddametyy@gmail.com>
2024-12-19 17:21:25 +0900

    Update about Handsign components

--- src/components/hand-sign/organisms/HandSignWrapper/index.tsx
+++ src/components/hand-sign/organisms/HandSignWrapper/index.tsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Caution from '../../atoms/Caution'
 import Title from '../../atoms/Title'
 import HandSIgnContainer from '../../molecules/HandSIgnContainer'
 import * as S from './style'
+import { authInstance } from '@/api/axios'
 import Button from '@/components/common/Button'
 import Header from '@/components/Header'
 
-const HandSignWrapper = () => {
+const HandSignWrapper = ({ id }: { id: number }) => {
+  const [sign, setSign] = useState<string | null>(null)
+  const handlingClickEvent = async () => {
+    try {
+      const response = await authInstance.post(`/product/${id}/buy`, {
+        handSign: sign,
+      })
+      alert(response)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  console.log(sign)
   return (
     <S.Wrapper>
-      <Header />
+      <Header appearance={true} />
       <S.Content>
         <S.Container>
           <Title />
-          <HandSIgnContainer />
-          <Button type='min' text='선택하지 않고, 얼굴 인식' />
+          <HandSIgnContainer setSign={setSign} />
+          <Button type='min' text='확인' onClick={() => handlingClickEvent()} />
           <Caution />
         </S.Container>
       </S.Content>
